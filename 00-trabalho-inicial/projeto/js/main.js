590e6bdd497b6260361a557996382a18af84f0d5
Rodrigo Martins <contawarface750@gmail.com>
2018-04-23 20:47:22 -0300

    minify js and css

--- 00-trabalho-inicial/projeto/js/main.js
+++ 00-trabalho-inicial/projeto/js/main.js
@@ -1,8 +1,1 @@
-window.onpageshow = function() {
-    var snackbarContainer = document.querySelector('#demo-toast-example');
-    var data = {
-        message: 'Nossos os produtos são extraidos de pessoas que, nunca beberam e sempre fizeram dietas restritas, por isso temos os produtos mais puros.',
-        timeout: 6000
-    };
-    snackbarContainer.MaterialSnackbar.showSnackbar(data);
-}
+window.onpageshow = function() { const snackbarContainer = document.querySelector('#demo-toast-example'); const data = { message: 'Nossos os produtos são extraidos de pessoas que, nunca beberam e sempre fizeram dietas restritas, por isso temos os produtos mais puros.', timeout: 6000 }; snackbarContainer.MaterialSnackbar.showSnackbar(data) };
--- 00-trabalho-inicial/projeto/js/produto.js
+++ 00-trabalho-inicial/projeto/js/produto.js
@@ -1,7 +1,1 @@
-const QUANTIDADE = document.querySelector('#quantidade');
-const SPAN = document.querySelector('#valor');
-
-QUANTIDADE.addEventListener('change', function() {
-    let quantidade = parseInt(QUANTIDADE.value);
-    SPAN.textContent = 'R$ ' +(quantidade * 10) + ',00';
-});
+const QUANTIDADE = document.querySelector('#quantidade'); const SPAN = document.querySelector('#valor'); QUANTIDADE.addEventListener('change', function () { const quantidade = parseInt(QUANTIDADE.value); SPAN.textContent = 'R$ ' + (quantidade * 10) + ',00' })
