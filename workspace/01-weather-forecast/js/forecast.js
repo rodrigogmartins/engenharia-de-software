f6a66a1d126eaabb25bc4d092cf1b93defb30372
Rodrigo Martins <contawarface750@gmail.com>
2018-05-22 10:49:44 -0300

    Update forecast.js

--- workspace/01-weather-forecast/js/forecast.js
+++ workspace/01-weather-forecast/js/forecast.js
@@ -12,16 +12,16 @@ export const getCurrentPosition = function() {
     navigator.geolocation.getCurrentPosition(function(posicao) {
         const latitude = posicao.coords.latitude;
         const longitude = posicao.coords.longitude;
-        ajax(`http://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`,
+        ajax(`https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`,
             (e) => getCityId(JSON.parse(e.target.response).address.city,
                 ESTADOS.get(JSON.parse(e.target.response).address.state)));
     });
 };
 
 const getCityId = (cityName, state) =>
-    ajax(`http://apiadvisor.climatempo.com.br/api/v1/locale/city?name=${cityName}&state=${state}&token=${CLIMATEMPOTOKEN}`,
+    ajax(`https://apiadvisor.climatempo.com.br/api/v1/locale/city?name=${cityName}&state=${state}&token=${CLIMATEMPOTOKEN}`,
         (e) => getWeatherForecast(JSON.parse(e.target.response)[0].id));
 
 const getWeatherForecast = (cityId) =>
-    ajax(`http://apiadvisor.climatempo.com.br/api/v1/forecast/locale/${cityId}/days/15?token=${CLIMATEMPOTOKEN}`,
+    ajax(`https://apiadvisor.climatempo.com.br/api/v1/forecast/locale/${cityId}/days/15?token=${CLIMATEMPOTOKEN}`,
         (e) => localStorage.setItem('previsao', e.target.response));
